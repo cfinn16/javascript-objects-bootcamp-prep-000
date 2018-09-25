109f0d11e6cae00f7b6f8a35346f8ac099187ef4
Connor Finnegan <connorfinnegan16@gmail.com>
2018-09-25 04:20:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {"The Beatles": "Come Together", "Led Zeppelin": "Black Dog", "Rolling Stones": "Satisfaction"};
+var playlist = {["The Beatles"]: "Come Together", ["Led Zeppelin"]: "Black Dog", ["Rolling Stones"]: "Satisfaction"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
